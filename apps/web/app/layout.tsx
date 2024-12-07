d538701e55eb49dab303a151aae391dd33eb8644
Vien Pham <viendev@outlook.com>
2024-12-07 13:01:12 +0700

    feat: add packages shadcn and integrate with app web

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import "@repo/shadcn/global.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
