ffd2e94257c7cf069e0603af0cd9836d9b622480
Vien Pham <viendev@outlook.com>
2024-12-09 10:06:27 +0700

    fix: update shadcn globals.css

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,29 +1,11 @@
-import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/shadcn/components/ui/button";
-import styles from "./page.module.css";
 import { Input } from "@repo/shadcn/components/ui/input";
-
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main>
-        <Input className="rounded" />
+    <div>
+      <main className="container mx-auto">
+        <Input />
         <Button variant={"ghost"} className="text-red-500">
           Shadcn Button
         </Button>
